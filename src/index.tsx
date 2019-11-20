3432cc8e169d5a46ad957117ad7ac169c57727ab
Thomas Flemming <thomas.flemming@gmail.com>
2019-11-20 13:50:25 +0100

    disable redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,17 @@
 import * as React from "react";
 import { render } from "react-dom";
-import {Provider} from "react-redux";
-
 import DetteBorDuVite from "./artikler/dette-bor-du-vite/DetteBorDuVite";
-import configureStore from "./redux/configureStore";
 import "./styles.css";
 import "./index.less";
 
-const store = configureStore();
-
 function App() {
   return (
-      <Provider store={store}>
         <div>
             <br/>
             <br/>
             <DetteBorDuVite/>
         </div>
-      </Provider>
+
   );
 }
 
